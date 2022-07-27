57681b2d4223b488b9b074cec614cc2794562d02
v-siyongyuan <v-siyongyuan@microsoft.com>
2022-07-27 16:30:15 +0800

    gitdev/remote branch 修改内容

--- Adam.WebApi/Program.cs
+++ Adam.WebApi/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // 改点内容吧-devmaster branch
+// gitdev/remote 修改内容
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
