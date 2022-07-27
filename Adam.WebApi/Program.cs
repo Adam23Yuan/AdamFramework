edd0d90225f42ed7e1c6f7905755394539ab5245
v-siyongyuan <v-siyongyuan@microsoft.com>
2022-07-27 17:37:53 +0800

    this is a commit description A B D E F G :Q

--- Adam.WebApi/Program.cs
+++ Adam.WebApi/Program.cs
@@ -13,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // gitdev/remote 第三次修改内容
 // gitdev/remote 第四次修改内容
 // test git diff cmd
+// test git diff cmd not commmit
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
