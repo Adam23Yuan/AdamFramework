4ee4fd954ba5ee1127a18304e6bb497b709038fe
v-siyongyuan <v-siyongyuan@microsoft.com>
2022-08-23 10:47:02 +0800

    build git diff patch commit file

--- EFCoreApp/Program.cs
+++ EFCoreApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// git diff patch commit
+Console.WriteLine($"build git diff patch commit file");
 // git diff path uncommit
 string inputContent = Console.ReadLine();
