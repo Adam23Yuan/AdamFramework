2eaa61379975921fbf0e928d4b69737d24317fe0
v-siyongyuan <v-siyongyuan@microsoft.com>
2022-08-23 13:47:51 +0800

    udpate

--- EFCoreApp/Program.cs
+++ EFCoreApp/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// git diff patch commit
+// git diff patch commit file
 Console.WriteLine($"build git diff patch commit file");
-// git diff patch commit
+// git diff patch commit second
 Console.WriteLine($"build git diff patch commit file second");
-// git diff patch commit
-Console.WriteLine($"build git diff patch commit file");
+// git diff patch commit third
+Console.WriteLine($"build git diff patch commit file third");
 // git diff path uncommit
 string inputContent = Console.ReadLine();
