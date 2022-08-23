a857055eb2bba75f3ea6f7fde5aeb9d1838eff24
v-siyongyuan <v-siyongyuan@microsoft.com>
2022-08-23 10:50:26 +0800

    again

--- EFCoreApp/Program.cs
+++ EFCoreApp/Program.cs
@@ -2,5 +2,7 @@
 Console.WriteLine("Hello, World!");
 // git diff patch commit
 Console.WriteLine($"build git diff patch commit file");
+// git diff patch commit
+Console.WriteLine($"build git diff patch commit file second");
 // git diff path uncommit
 string inputContent = Console.ReadLine();
