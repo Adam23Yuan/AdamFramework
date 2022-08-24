68bd2b04699c845ed39420814ea252716c480ab0
v-siyongyuan <v-siyongyuan@microsoft.com>
2022-08-24 11:19:11 +0800

    udpate

--- Adam.DbContexts/ApplicationDbContext.cs
+++ Adam.DbContexts/ApplicationDbContext.cs
@@ -56,33 +56,55 @@ namespace Adam.WebApi.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // 集中配置 分组配置 选择一种即可
-            // 1、集中配置
-            //modelBuilder.Entity<Blog>().Property(b => b.Url).IsRequired();
-            // 2、分组配置【为了减小OnModelCreate方法的大小】
-            new BlogEntityTypeConfiguration().Configure(modelBuilder.Entity<Blog>());
-            // 3、在实体属性上通过特性配置
-
-            // 动态添加模型
-            modelBuilder.Entity<AuditEntry>();
-            // 映射表名称+架构名 或者通过在实体类的特性上指定
-            modelBuilder.Entity<Blog>().ToTable("Blogs", schema: "dbo").HasComment("Blogs managed on the website");
-
-            // 视图映射 
-            modelBuilder.Entity<Blog>().ToView("blogsView", schema: "dbo");
-
-            // 表值函数映射 
-            // note 若要将实体映射到表值函数，函数必须是无参数的。
-            modelBuilder.Entity<BlogWithMultiplePosts>().HasNoKey().ToFunction("BlogsWithMultiplePosts");
-
-            // 共享类型实体类型
-            modelBuilder.SharedTypeEntity<Dictionary<string, object>>(
-            "Blog", bb =>
+            #region 实体类型相关
             {
-                bb.Property<int>("BlogId");
-                bb.Property<string>("Url");
-                bb.Property<DateTime>("LastUpdated");
-            });
+                // 集中配置 分组配置 选择一种即可
+                // 1、集中配置
+                //modelBuilder.Entity<Blog>().Property(b => b.Url).IsRequired();
+                // 2、分组配置【为了减小OnModelCreate方法的大小】
+                new BlogEntityTypeConfiguration().Configure(modelBuilder.Entity<Blog>());
+                // 3、在实体属性上通过特性配置
+
+                // 4、动态添加模型
+                modelBuilder.Entity<AuditEntry>();
+                // 映射表名称+架构名 或者通过在实体类的特性上指定
+                modelBuilder.Entity<Blog>().ToTable("Blogs", schema: "dbo").HasComment("Blogs managed on the website");
+
+                // 5、视图映射 
+                modelBuilder.Entity<Blog>().ToView("blogsView", schema: "dbo");
+
+                // 表值函数映射 
+                // note 若要将实体映射到表值函数，函数必须是无参数的。
+                modelBuilder.Entity<BlogWithMultiplePosts>().HasNoKey().ToFunction("BlogsWithMultiplePosts");
+
+                // 6、共享类型实体类型
+                modelBuilder.SharedTypeEntity<Dictionary<string, object>>(
+                "Blog", bb =>
+                {
+                    bb.Property<int>("BlogId");
+                    bb.Property<string>("Url");
+                    bb.Property<DateTime>("LastUpdated");
+                });
+            }
+            #endregion
+
+            #region 实体属性相关
+            {
+                // 1、排除实体属性 将不和数据库列映射，也可以通过实体特性特性指定 [NotMapped]
+                modelBuilder.Entity<Blog>().Ignore(o => o.LoadedFromDatabase);
+
+                // 2、列名映射 也可以通过实体特性特性指定 [Column("blog_id")]
+                modelBuilder.Entity<Blog>().Property(o => o.BlogId).HasColumnName("blog_id");
+
+                // 3、数据库类型 也可以通过实体特性特性指定 [Column("blog_id")]
+                modelBuilder.Entity<Blog>().Property(o => o.Rating).HasColumnType("decimal(5, 2)");
+                modelBuilder.Entity<Blog>(eb =>
+                {
+                    eb.Property(o => o.Rating).HasColumnType("decimal(5, 2)");
+                });
+
+            }
+            #endregion
         }
     }
 }
--- Adam.Models/Blog.cs
+++ Adam.Models/Blog.cs
@@ -6,8 +6,8 @@ namespace Adam.Models
 {
     /// <summary>
     /// 特性说明
-    /// <para>Table 使用特性指定表名加 框架名 也可以在 OnModelCreateing使用 ToTable 中指定</para>
-    /// <para>Comment 指定表注释 也可以在 OnModelCreateing中使用 HasComment 指定</para>
+    /// <para>1.Table 使用特性指定表名加 框架名 也可以在 OnModelCreating 使用 ToTable 中指定</para>
+    /// <para>2.Comment 指定表注释 也可以在 OnModelCreating 中使用 HasComment 指定</para>
     /// </summary>
     [Table("Blogs", Schema = "dbo")]
     [Comment("Blogs managed on the website")]
@@ -15,7 +15,10 @@ namespace Adam.Models
     {
         /// <summary>
         /// 
+        /// <para>映射数据库列名</para>
+        /// <para>1.也可以在  OnModelCreating 方法中 通过属性指定 HasColumnName</para>
         /// </summary>
+        [Column("blog_id")]
         public int BlogId { get; set; }
 
         /// <summary>
@@ -24,6 +27,23 @@ namespace Adam.Models
         [Required]
         public string Url { get; set; }
 
+        /// <summary>
+        /// <para>数据库类型</para>
+        /// <para>1.也可以在  OnModelCreating 方法中 通过属性指定 </para>
+        /// </summary>
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal Rating { get; set; }
+
+        /// <summary>
+        /// 排除属性
+        /// <para>1.也可以在  OnModelCreating 方法中 排除</para>
+        /// </summary>
+        [NotMapped]
+        public DateTime LoadedFromDatabase { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public List<Post> Posts { get; set; }
     }
 }
